b638adff7ca58328155bd4ff31197211992f18a7
Kenny Diogo <kenneth.diogo@gmail.com>
2022-12-04 02:53:48 -0500

    Redesigned project pages and updated Waru and EXT2EC pages

---
+++ src/components/shared/ProjectTechnologyItem.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+export type TechnologyItemProps = {
+  name: string;
+  description: string;
+  imgSrc: string;
+};
+
+defineProps<TechnologyItemProps>();
+</script>
+
+<template>
+  <div class="bg-darcula-600 flex px-2 py-1 gap-2 rounded items-center">
+    <div class="w-12 h-12 p-2 flex-grow-0 flex-shrink-0">
+      <img class="w-full h-full" :src="imgSrc" />
+    </div>
+    <div class="flex flex-col">
+      <span class="text-sm">{{ name }}</span>
+      <span class="text-xs text-darcula-300">{{ description }}</span>
+    </div>
+  </div>
+</template>
